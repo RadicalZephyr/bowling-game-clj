f676f7f6a7ef274d3c1372bd15f3f6969b713730
Geoff Shannon <geoffpshannon@gmail.com>
2016-10-04 15:30:40 -0500

    After Zagaku live-coding

--- src/bowling_game/core.clj
+++ src/bowling_game/core.clj
@@ -1,6 +1,50 @@
-(ns bowling-game.core)
+(ns bowling-game.core
+  (:require [clojure.tools.trace :as t]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn new-game []
+  [])
+
+(defn roll [game pins]
+  (conj game pins))
+
+(defn- sum [coll]
+  (apply + coll))
+
+(defn- spare? [frame]
+  (= 10 (sum frame)))
+
+(defn- number-of-rolls-in-frame [frame]
+  (if (spare? frame)
+    3
+    2))
+
+(defn frameseq [game]
+  (lazy-seq
+   (let [frame (take 2 game)]
+     (cons (take (number-of-rolls-in-frame)
+                 game)
+          (frameseq (drop 2 game))))))
+
+(defn score-game [game]
+  (->> game
+       frameseq
+       (take 10)
+       (map sum)
+       sum))
+
+(defn- fibonacci-seq [n]
+  (cond
+    (= n 0) 0
+    (= n 1) 1
+    :else (+(fibonacci-seq (- n 1))
+            (fibonacci-seq (- n 2)))))
+
+(defn- fib-seq-iter [n1 n2]
+  (lazy-seq
+   (let [next (+ n1 n2)]
+     (cons next
+           (fib-seq-iter n2 next)))))
+
+(defn fibonacci-seq []
+  (concat [0 1]
+          (fib-seq-iter 0 1)))
--- test/bowling_game/core_test.clj
+++ test/bowling_game/core_test.clj
@@ -2,6 +2,33 @@
   (:require [clojure.test :refer :all]
             [bowling-game.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-empty-game
+  (is (= 0 (score-game (new-game)))))
+
+(deftest test-singles-game
+  (let [game (new-game)
+        updated-game (reduce roll game (repeat 20 1))]
+    (is (= 20 (score-game updated-game)))))
+
+(deftest test-one-spare
+  (let [game (new-game)
+        rolls (concat [5 5] ;; spare
+                      [3]
+                      (repeat 17 0))
+        updated-game (reduce roll game rolls)]
+    (is (= 16 (score-game updated-game)))))
+
+(deftest test-fibonacci
+  (is (= [0]
+         (take 1 (fibonacci-seq))))
+
+  (is (= [0 1]
+         (take 2 (fibonacci-seq))))
+
+  (is (= [0 1 1]
+         (take 3 (fibonacci-seq))))
+  (is (= [0 1 1 2]
+         (take 4 (fibonacci-seq))))
+
+  #_(is (= [0 1 1 2 3 5 8 13 21]
+           (take 9 (fibonacci-seq)))))
